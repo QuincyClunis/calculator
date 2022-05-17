a3d65fe433f43d6335b12b986a50a7898c2cb0ff
Ignacy Adamczyk <iadamczyk1@st.swps.edu.pl>
2022-05-18 00:13:01 +0200

    kalkulator działa brzydko, ale przynajmniej liczy

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const numberButtons = document.querySelectorAll('.numbers')
 const equalsButton = document.querySelector('.equals')
 const allClearButton = document.querySelector('.clear')
@@ -7,12 +8,73 @@ const additionButton = document.querySelector('.addition')
 const subtractionButton = document.querySelector('.subtraction')
 const output = document.querySelector('.output')
 
+let firstNumber = ''
+let secondNumber = ''
+let operation = ''
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         output.innerText = output.innerText.toString() + button.innerText.toString()
+        if (firstNumber !== ''){
+            secondNumber = output.innerText
+        }
     })
 })
 
 allClearButton.addEventListener('click', () => {
     output.innerText = ''
+    firstNumber = ''
+    secondNumber = ''
 })
+
+
+multiplyButton.addEventListener('click', () => {
+    firstNumber = output.innerText
+    operation = '*'
+    output.innerText = ''
+})
+
+divisionButton.addEventListener('click', () => {
+    firstNumber = output.innerText
+    operation = '/'
+    output.innerText = ''
+})
+
+additionButton.addEventListener('click', () => {
+    firstNumber = output.innerText
+    operation = '+'
+    output.innerText = ''
+})
+
+subtractionButton.addEventListener('click', () => {
+    firstNumber = output.innerText
+    operation = '-'
+    output.innerText = ''
+})
+
+
+
+equalsButton.addEventListener('click', () => {
+    let computation
+    switch (operation) {
+        case '+':
+            computation = parseFloat(firstNumber) + parseFloat(secondNumber)
+            break
+        case '-':
+            computation = parseFloat(firstNumber) - parseFloat(secondNumber)
+            break
+        case '*':
+            computation = parseFloat(firstNumber) * parseFloat(secondNumber)
+            break
+        case '/':
+            computation = parseFloat(firstNumber) / parseFloat(secondNumber)
+            break
+    }
+
+    output.innerText = computation
+    firstNumber = ''
+    secondNumber = ''
+
+})
+
+
